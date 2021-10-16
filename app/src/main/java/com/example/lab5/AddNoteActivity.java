3dccc187dd1227165b1641e9d3b0412b22ef53ec
Riley Larget <larget@wisc.edu>
2021-10-16 15:24:42 -0500

    Milestone 2

---
+++ app/src/main/java/com/example/lab5/AddNoteActivity.java
@@ -0,0 +1,69 @@
+package com.example.lab5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class AddNoteActivity extends AppCompatActivity {
+
+    private int noteid = -1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+
+        // 1. get EditText view
+        EditText editNote = (EditText) findViewById(R.id.addNoteText);
+        // 2. get Intent
+        Intent intent = getIntent();
+        // 3. get the value of the integer "noteid" from intent
+        // 4. initialize class variable noteid with value from intent
+        noteid = intent.getIntExtra("noteid",-1);
+
+        Log.i("NoteID","Note id:"+noteid);
+        if (noteid != -1) {
+            Note note = WelcomeActivity.notes.get(noteid);
+            String noteContent = note.getContent();
+            editNote.setText(noteContent);
+        }
+    }
+
+    public void saveNote(View view) {
+        // 1. get editText view and the content that the user entered
+        EditText editNote = (EditText) findViewById(R.id.addNoteText);
+        String content = editNote.getText().toString();
+        // 2. Initialize SQLiteDatabase instance
+        // 3. Initialize DBHelper class
+        DBHelper helper = new DBHelper(getApplicationContext());
+
+        // 4. get username from SharedPreferences
+        SharedPreferences sharedPreferences = getSharedPreferences("com.example.lab5", Context.MODE_PRIVATE);
+        String username = sharedPreferences.getString(MainActivity.usernameKey,"");
+
+        // 5. save information to DB
+        String title;
+        DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        String date = dateFormat.format(new Date());
+
+        if (noteid == -1) { // add note
+            title = "NOTE_" + (WelcomeActivity.notes.size()+1);
+            helper.saveNotes(username,title,content,date);
+        } else {
+            title = "NOTE_" + (noteid+1);
+            helper.updateNotes(title,date,username,content);
+        }
+        // 6. Go to second activity using intents
+        Intent intent = new Intent(getApplicationContext(),WelcomeActivity.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/example/lab5/WelcomeActivity.java
+++ app/src/main/java/com/example/lab5/WelcomeActivity.java
@@ -6,27 +6,66 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class WelcomeActivity extends AppCompatActivity {
 
     private TextView welcome;
+    public static ArrayList<Note> notes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
+        // 1. Display welcome message. Fetch username from SharedPreferences.
         welcome = (TextView) findViewById(R.id.welcomeText);
-        Intent intent = getIntent();
-//        String username = intent.getStringExtra("username");
+
         SharedPreferences sharedPreferences = getSharedPreferences("com.example.lab5",Context.MODE_PRIVATE);
         String username = sharedPreferences.getString(MainActivity.usernameKey,"");
+
         welcome.setText("Welcome "+username+"!");
+
+        // 2. Get SQLiteDatabase Instance
+        Context context = getApplicationContext();
+        SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("notes",Context.MODE_PRIVATE,null);
+        DBHelper notesDB = new DBHelper(sqLiteDatabase);
+
+        // 3. Initiate the "notes" class variable using readNotes method in DBHelper class using username from SharedPreferences
+        notes = notesDB.readNotes(username);
+
+        // 4. Create an ArrayList<String> by iterating over notes object
+        ArrayList<String> displayNotes = new ArrayList<>();
+        for (Note note: notes) {
+            displayNotes.add(String.format("Title:%s\nDate:%s",note.getTitle(),note.getDate()));
+        }
+
+        // 5. Use ListView view to display notes on screen
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,displayNotes);
+        ListView listView = (ListView) findViewById(R.id.notesList);
+        listView.setAdapter(adapter);
+
+        // 6. Add onItemClickListener for ListView item
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), AddNoteActivity.class);
+                intent.putExtra("noteid",position);
+                startActivity(intent);
+            }
+        });
+
     }
 
     private void logout() {
@@ -47,10 +86,19 @@ public class WelcomeActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         super.onOptionsItemSelected(item);
 
-        if (item.getItemId()==R.id.logout) {
-            logout();
-            return true;
+        switch (item.getItemId()){
+            case R.id.logout:
+                logout();
+                break;
+            case R.id.addNote:
+                addNewNote();
+                break;
         }
-        return false;
+        return true;
+    }
+
+    private void addNewNote() {
+        Intent intent = new Intent(this,AddNoteActivity.class);
+        startActivity(intent);
     }
 }
